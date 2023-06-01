f026155242e5076f3c8ca9d2e5c5d5d26b7d2350
amanr <amanrique068@gmail.com>
2023-05-31 22:13:58 -0500

    validaciones de e4

--- src/main/java/com/example/lab7_20203248/Controller/AccionesController.java
+++ src/main/java/com/example/lab7_20203248/Controller/AccionesController.java
@@ -1,7 +1,9 @@
 package com.example.lab7_20203248.Controller;
 
 import com.example.lab7_20203248.Entity.Acciones;
+import com.example.lab7_20203248.Entity.Usuarios;
 import com.example.lab7_20203248.Repository.AccionesRepository;
+import com.example.lab7_20203248.Repository.UsuariosRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +11,7 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -16,16 +19,52 @@ import java.util.HashMap;
 public class AccionesController {
 
     final AccionesRepository accionesRepository;
+    final UsuariosRepository usuariosRepository;
 
-    AccionesController(AccionesRepository accionesRepository){
+    AccionesController(AccionesRepository accionesRepository, UsuariosRepository usuariosRepository){
         this.accionesRepository = accionesRepository;
+        this.usuariosRepository = usuariosRepository;
     }
 
     @PostMapping("/save")
     public ResponseEntity<HashMap<String, Object>> guardar(@RequestBody Acciones acciones){
 
         HashMap<String, Object> responesMap = new HashMap<>();
-        accionesRepository.guardar(acciones.getId(), acciones.getMonto(), acciones.getFecha(), acciones.getUsuarios_id().getId());
+        List<Acciones> list = accionesRepository.findAll();
+        List<Usuarios> list1 = usuariosRepository.findAll();
+
+        boolean checkAccion = true;
+        for (Acciones a: list){
+            if(a.getId().equals(acciones.getId())){
+                checkAccion = false;
+                break;
+            }
+        }
+
+        boolean checkUser = false;
+        for (Usuarios u: list1){
+            if (u.getId() == acciones.getUsuarios_id().getId()){
+                checkUser = true;
+                break;
+            }
+        }
+        if (!checkUser || !checkAccion || acciones.getMonto()==null || acciones.getFecha()==null){
+            responesMap.put("result", "error");
+            if (!checkUser){
+                responesMap.put("msg","Debe ingresar un usuario existente");
+            }
+            if (!checkAccion){
+                responesMap.put("msg","Debe ingresar una accion existente");
+            }
+            if (acciones.getMonto()==null){
+                responesMap.put("msg","Se requiere un monto");
+            }
+            if (acciones.getFecha()==null){
+                responesMap.put("msg","Se requiere una fecha");
+            }
+        }
+
+        accionesRepository.save(acciones);
         responesMap.put("idCreado", acciones.getUsuarios_id().getId());
         return ResponseEntity.status(HttpStatus.CREATED).body(responesMap);
 
