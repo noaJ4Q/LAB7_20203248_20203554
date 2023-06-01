ecf71ed841a05f54aff20a6c917a27da70a77f99
amanr <amanrique068@gmail.com>
2023-05-31 21:48:15 -0500

    excpetionhandler

--- src/main/java/com/example/lab7_20203248/Controller/PagosController.java
+++ src/main/java/com/example/lab7_20203248/Controller/PagosController.java
@@ -2,8 +2,10 @@ package com.example.lab7_20203248.Controller;
 
 import com.example.lab7_20203248.Entity.Pagos;
 import com.example.lab7_20203248.Repository.PagosRepository;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -33,5 +35,14 @@ public class PagosController {
         respuesta.put("id", pago.getId());
         return ResponseEntity.status(HttpStatus.CREATED).body(respuesta); //para respetar RESTful hay un codigo por metodo
 
+    }
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<HashMap<String, String>> gestionCrear(HttpServletRequest request){
+        HashMap<String, String> responseMap = new HashMap<>();
+        if (request.getMethod().equals("POST") || request.getMethod().equals("PUT") || request.getMethod().equals("DELETE")){
+            responseMap.put("estado", "error");
+            responseMap.put("msg", "Parámetro(s) nulos");
+        }
+        return ResponseEntity.badRequest().body(responseMap);
     }
 }
