ad7e8273eb92b8d0655cbef79465891fccd65e0c
amanr <amanrique068@gmail.com>
2023-05-31 19:48:10 -0500

    agregando validaciones y creación de nuevos roles cuando se ingresa uno no existente en el raw

--- src/main/java/com/example/lab7_20203248/Controller/UsuariosController.java
+++ src/main/java/com/example/lab7_20203248/Controller/UsuariosController.java
@@ -1,6 +1,8 @@
 package com.example.lab7_20203248.Controller;
 
+import com.example.lab7_20203248.Entity.Rol;
 import com.example.lab7_20203248.Entity.Usuarios;
+import com.example.lab7_20203248.Repository.RolRepository;
 import com.example.lab7_20203248.Repository.UsuariosRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -18,9 +20,11 @@ import java.util.List;
 public class UsuariosController {
 
     final UsuariosRepository usuariosRepository;
+    final RolRepository rolRepository;
 
-    UsuariosController(UsuariosRepository usuariosRepository){
+    UsuariosController(UsuariosRepository usuariosRepository, RolRepository rolRepository){
         this.usuariosRepository = usuariosRepository;
+        this.rolRepository = rolRepository;
     }
 
     @GetMapping(value = "/listar", produces = MediaType.APPLICATION_JSON_VALUE+"; charset=utf-8")
@@ -32,10 +36,42 @@ public class UsuariosController {
     public ResponseEntity<HashMap<String, Object>> crear(@RequestBody Usuarios usuarios){
 
         HashMap<String, Object> responseMap = new HashMap<>();
-        usuariosRepository.guardar(usuarios.getId(), usuarios.getNombre(), usuarios.getApellido(), usuarios.getCorreo(), usuarios.getUsername(), usuarios.getPassword(), usuarios.getEstado_logico(), usuarios.getRol_id().getId(), usuarios.getFecha_registro());
+        List<Usuarios> lista = usuariosRepository.findAll();
+        List<Rol> listaR = rolRepository.findAll();
+        HashMap<String, Object> hashMap = new HashMap<>();
+        boolean checkId = true;
+        boolean checkRol = true;
+        for (Usuarios u: lista){
+            if(u.getId()==usuarios.getId()){
+                checkId = false;
+            }
+        }
+        int contador = 0;
+        for (Rol r: listaR){
+            if(r.getId()==usuarios.getRol_id().getId()){
+                if(!r.getNombre_rol().equals(usuarios.getRol_id().getNombre_rol())) {
+                    checkRol = false;
+                }
+                contador ++;
+            }
+        }
+        if(!checkId || usuarios.getRol_id()==null || usuarios.getRol_id().getId()==null || !checkRol){
+            hashMap.put("result", "error");
+            if(!checkId) {
+                hashMap.put("msg", "Ingrese un id válido");
+            }
+            if(usuarios.getRol_id()==null || !checkRol || usuarios.getRol_id().getId()==null){
+                hashMap.put("msg", "Ingrese un rol válido");
+            }
+            return ResponseEntity.badRequest().body(hashMap);
+        }
+        if(contador == 0){
+            rolRepository.save(usuarios.getRol_id());
+        }
+        //usuariosRepository.guardar(usuarios.getId(), usuarios.getNombre(), usuarios.getApellido(), usuarios.getCorreo(), usuarios.getUsername(), usuarios.getPassword(), usuarios.getEstado_logico(), usuarios.getRol_id().getId(), usuarios.getFecha_registro());
+        usuariosRepository.save(usuarios);
         responseMap.put("id creado:", usuarios.getId());
         return ResponseEntity.status(HttpStatus.CREATED).body(responseMap);
-
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
